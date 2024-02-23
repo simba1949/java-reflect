061b24ac721611f5cef15730d7dce4c148801b47
anthony <anthony1949@zto.com>
2024-02-23 15:48:46 +0800

    feature：添加 filed 操作

---
+++ java-reflect-quick-start/src/main/java/vip/openpark/jdk/field/N1FieldApplication.java
@@ -0,0 +1,39 @@
+package vip.openpark.jdk.field;
+
+import vip.openpark.jdk.common.Human;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * @author anthony
+ * @version 2024/2/23 15:34
+ */
+public class N1FieldApplication {
+	public static void main(String[] args) throws IllegalAccessException {
+		Human human = new Human();
+		human.setName("anthony");
+		human.setAge(18);
+		
+		Field[] fields = Human.class.getDeclaredFields();
+		for (Field field : fields) {
+			// 设置可访问
+			field.setAccessible(true);
+			
+			// 获取名称
+			String name = field.getName();
+			System.out.println("获取名称：" + name);
+			// 获取类型
+			Class<?> type = field.getType();
+			System.out.println("获取类型：" + type);
+			// 获取修饰符 {@code Modifier}
+			int modifiers = field.getModifiers();
+			System.out.println("获取修饰符：" + Modifier.toString(modifiers));
+			
+			// 获取值
+			System.out.println("获取值：" + field.get(human));
+			// 设置值
+			field.set(human, "123456");
+		}
+	}
+}
