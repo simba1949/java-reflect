3126cb689fe0196ad434fe74021d8c62464bd153
anthony <openpark.vip@outlook.com>
2025-07-08 23:09:35 +0800

    style：添加注释

--- annotation-data/annotation-data-with-auto-service-example/src/main/java/vip/openpark/annotation/AnnotationApplication.java
+++ annotation-data/annotation-data-with-auto-service-example/src/main/java/vip/openpark/annotation/AnnotationApplication.java
@@ -6,6 +6,9 @@ package vip.openpark.annotation;
  * @since 2025/7/8 23:00
  */
 public class AnnotationApplication {
+	/**
+	 * 导入代码时，ide 会报错的，但是编译完成之后 ide 会找到对应的 class 文件，可以正常运行
+	 */
 	public static void main(String[] args) {
 		HumanDataGen humanDataGen = new HumanDataGen();
 		humanDataGen.setName("anthony");
